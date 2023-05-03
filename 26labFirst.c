7907cec9863e2d8cf1f0e975d8a5920a5d89aa1c
Prakash rathod <111631830+Prakashrathod15@users.noreply.github.com>
2023-05-03 21:43:29 +0530

    Add files via upload

---
+++ 26labFirst.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+	
+int a,b,sum,multi,div,mod;
+a = 10;
+b = 20;
+sum = a+b;
+printf("%d",a);
+printf("\n %d",b);
+printf("\n sum is =%d",sum);	
+multi = a*b;
+div = b/a;
+mod = b%a;
+printf("\n multi is =%d",multi);
+printf("\n div is =%d",div);
+printf("\n mod is =%d",mod);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
