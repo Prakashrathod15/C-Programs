cd315d5af491ac8d62a7f3be0d796ca15916da00
Prakash rathod <111631830+Prakashrathod15@users.noreply.github.com>
2023-05-04 00:32:37 +0530

    Add files via upload

---
+++ patternPrinting.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,j;
+	printf("\n Enter number ");           
+	scanf("%d",&n);                      
+	while(n>=i)                          
+	{     
+	     j = 1;                            
+		 while(j<=10)
+		 {
+							  }                         
+	}
+	
+}
