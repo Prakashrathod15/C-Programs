0942945547cea5d099d19ca3d49b9e7d9c423b18
Prakash rathod <111631830+Prakashrathod15@users.noreply.github.com>
2023-05-03 23:29:07 +0530

    Add files via upload

---
+++ Area_And_Perimeter_of_Reactangle_.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	float area, len, breath, perimeter;
+	len = 5;
+	breath = 4;
+	area = (len*breath);
+	perimeter = 2*(len+breath);
+	printf("The area of reactangle is =%f",area);
+	printf("\nThe perimeter of reactangle is =%f",perimeter);
+	
+	
+	
+	
+}
